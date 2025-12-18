f73dd71c66f30740b5d68d8275d2eb27bca23604
bala-amypo <bala@amypo.in>
2025-12-18 05:27:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.UserModel;
+import com.example.demo.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    UserService userService;
+    @PostMapping("/add")
+    public UserModel add(@RequestBody UserModel user){
+        return userService.createUser(user);
+    }
+    @GetMapping("/all")
+    public List<UserModel> show(){
+        return userService.getAllUsers();
+    }
+}
---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.UserModel;
+
+public interface UserRepository extends JpaRepository<UserModel, Long>{
+    
+}
