13e32b08d57a5a1f898430fa2ffa6c11a500b376
bala-amypo <bala@amypo.in>
2025-12-20 19:55:11 +0000

    User commit

--- src/main/java/com/example/demo/service/DuplicateDetectionLogService.java
+++ src/main/java/com/example/demo/service/DuplicateDetectionLogService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.DuplicateDetectionLog;
 
-public interface DuplicateDetectionService {
+public interface DuplicateDetectionLogService {
     List<DuplicateDetectionLog> detectDuplicates(Long ticketId);
     List<DuplicateDetectionLog> getLogsForTicket(Long ticketId);
     DuplicateDetectionLog getLog(Long id);
