108428b9e083d715508d3801954ad8f709389055
bala-amypo <bala@amypo.in>
2025-12-20 18:40:12 +0000

    User commit

--- src/main/java/com/example/demo/repository/DuplicateRuleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.DuplicateRuleModel;
-
-public interface DuplicateRuleRepository
-        extends JpaRepository<DuplicateRuleModel, Long> {
-}
