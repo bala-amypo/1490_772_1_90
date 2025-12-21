f35e5c5d7cf684a163cfb21b113d55a8d3ef2e98
bala-amypo <bala@amypo.in>
2025-12-21 16:15:40 +0000

    User commit

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,10 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
@@ -19,16 +16,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(User user) {
-        if (repo.existsByEmail(user.getEmail())) {
-            throw new IllegalArgumentException("email already exists");
-        }
+        if (repo.existsByEmail(user.getEmail())) return null;
         return repo.save(user);
     }
 
     @Override
     public User getUser(Long id) {
-        return repo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("user not found"));
+        return repo.findById(id).orElse(null);
     }
 
     @Override
