3ec3ca5e0112acc82f77082dd355dfbba6f7f5bf
bala-amypo <bala@amypo.in>
2025-12-26 12:15:15 +0000

    User commit

--- src/main/java/com/example/demo/service/DuplicateDetectionLogService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import java.util.List;
-import com.example.demo.model.DuplicateDetectionLog;
-
-public interface DuplicateDetectionLogService {
-    List<DuplicateDetectionLog> getByTicketId(Long ticketId);
-}
--- src/main/java/com/example/demo/service/TicketService.java
+++ src/main/java/com/example/demo/service/TicketService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
 
-import java.util.List;
 import com.example.demo.model.Ticket;
+import java.util.List;
 
 public interface TicketService {
-    Ticket save(Ticket ticket);
-    List<Ticket> getAll();
+    Ticket createTicket(Long userId, Long categoryId, Ticket ticket);
+    Ticket getTicket(Long id);
+    List<Ticket> getAllTickets();
+    List<Ticket> getTicketsByUser(Long userId);
 }
