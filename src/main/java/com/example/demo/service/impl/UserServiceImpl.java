0d8595436e154b47e51e15e33b5fe598408582c4
bala-amypo <bala@amypo.in>
2025-12-29 08:16:07 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -3,36 +3,25 @@ package com.example.demo.service.impl;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
- private final UserRepository userRepository;
-private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
-public UserServiceImpl(UserRepository userRepository,
-                       PasswordEncoder passwordEncoder) {
-    this.userRepository = userRepository;
-    this.passwordEncoder = passwordEncoder;
-}
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
 
     @Override
-public User registerUser(User user) {
-    if (userRepository.existsByEmail(user.getEmail())) {
-        throw new RuntimeException("Email already exists");
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+        return userRepository.save(user);
     }
-
-    user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-    if (user.getRole() == null) {
-        user.setRole("USER");
-    }
-
-    return userRepository.save(user);
-}
 
 
     @Override
